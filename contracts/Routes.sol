2a83999a27756e03aa4e9f06971f14f667cbe579
pbrudny <pbrudny@gmail.com>
2018-07-13 15:24:47 +0200

    Add test for new stop

--- contracts/Routes.sol
+++ contracts/Routes.sol
@@ -3,11 +3,13 @@ pragma solidity ^0.4.24;
 contract Routes {
     address public owner;   
 
+    struct Stop {
+        bytes32 name;
+        bytes10 latitude;
+        bytes10 longitude;    
+    }
+
     constructor() public {
         owner = msg.sender;
     }
-
-    function sayHello() public returns(string) {
-	return "Hello, World!";
-	}	
 }
--- test/RoutesTest.sol
+++ test/RoutesTest.sol
@@ -14,4 +14,8 @@ contract RoutesTest {
     function testSetOwner() public {
         Assert.equal(instance.owner(), address(this), "missing owner address");
     }
+
+    function testAddStop() public {
+        Assert.equal(instance.addStop("Cieszyn", "49.7629645", "18.6279841"), true, "Can not add new stop");
+    }
 }
